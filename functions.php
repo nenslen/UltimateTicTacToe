961e0fe46dbbe7387b15be15d260dc7f58571641
nenslen <nicolas.enslen@gmail.com>
2017-02-07 00:29:52 -0700

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+
+	function drawBoard() {
+		echo '<div class="board">';
+
+		// 3 Large rows
+		$id1 = 1;
+		for($i = 1; $i <= 3; $i++) {
+			echo '<div class="row">';
+
+			// 3 Large tiles
+			for($j = 1; $j <= 3; $j++) {
+				echo '<div class="largeTile" id="' . $id1 . '">';
+
+				// 3 Small rows
+				$id2 = 1;
+				for($k = 1; $k <= 3; $k++) {
+					echo '<div class="row">';
+
+					// 3 Small tiles
+					for($l = 1; $l <= 3; $l++) {
+						//echo '<div class="tile" id="' . $id1 . $id2++ .'">X</div>';
+						echo '<div class="tile" id="' . $id1 . $id2++ .'">X</div>';
+					}
+
+					echo '</div>';
+				}
+
+				$id1++;
+				echo '</div>';
+			}
+
+			echo '</div>';
+		}
+
+		echo '</div>';
+	}
+?>

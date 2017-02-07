485c68c8a1a8798d3613f6cc53d9558da49fd2a6
nenslen <nicolas.enslen@gmail.com>
2017-02-07 00:42:44 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<?php require 'functions.php'; ?>
+
+<html lang="en">
+<head>
+	<title>Ultimate Tic Tac Toe</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php drawBoard(); ?>
+</body>

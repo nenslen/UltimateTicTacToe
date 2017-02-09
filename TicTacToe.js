8d19f6e34c2ee7163a4478a77167e8251dc2e0c7
nenslen <nicolas.enslen@gmail.com>
2017-02-09 01:41:30 -0700

    Added game logic

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,8 +1,113 @@
-var TicTacToe = {
+function TicTacToe() {
+
+	this.board = new Array(9);
+	this.winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+	this.gameOver = false;
+
+
+	this.start = function() {
+		this.reset();
+	};
+
+
+	this.reset = function() {
+		currentPlayer = 'X';
+		for(var i = 1; i <= 9; i++) {
+			this.board[i - 1] = 'N';
+		}
+		gameOver = false;
+	};
+	
+
+	this.isValidMove = function(tile) {
+		if(this.board[tile - 1] === 'N') {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+
+	this.performTurn = function(tile, player) {
+		this.board[tile - 1] = player;
+	};
+
+
+
+	/*
+	 * Returns:
+	 * 'X' = X Wins
+	 * 'O' = O Wins
+	 * 'T' = Tie
+	 * 'N' = No winner yet
+	 */
+	this.checkWin = function() {
+
+		// Check for winning player
+		for(var i = 0; i < winningCombinations.length; i++) {
+			var tile1 = board[winningCombinations[i][0]];
+			var tile2 = board[winningCombinations[i][1]];
+			var tile3 = board[winningCombinations[i][2]];
+
+			if(tile1 == tile2 && tile2 == tile3 && tile1 != 'N') {
+				gameOver = true;
+				return tile1;
+			}
+		}
+
+
+		// Check for all tiles used
+		for(var i = 0; i < 9; i++) {
+			if(board[i] == 'N') {
+				return 'N';
+			}
+		}
+
+
+		// No winners and all tiles are used
+		gameOver = true;
+		return 'T'
+	};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function TicTacToe() {
 
 	board: new Array(9),
 	winningCombinations: [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
-	currentPlayer: 'X',
 	gameOver: false,
 
 
@@ -12,39 +117,62 @@ var TicTacToe = {
 
 
 	reset: function() {
-
 		currentPlayer = 'X';
 		for(var i = 1; i <= 9; i++) {
-			this.board[i - 1] = '';
+			this.board[i - 1] = 'N';
 		}
 		gameOver = false;
 	},
 	
 
 	isValidMove: function(tile) {
-		return board[tile - 1] == '';
+		if(this.board[tile - 1] === 'N') {
+			return true;
+		} else {
+			return false;
+		}
 	},
 
 
-	performTurn: function(tile) {
-		board[tile] = currentPlayer;
+	performTurn: function(tile, player) {
+		this.board[tile] = player;
 	},
 
 
-	swapTurns: function() {
-		if(currentPlayer == 'X') { currentPlayer = 'O'; }
-		else { currentPlayer = 'X'; }
-	},
 
-	// Returns true if a given player has won
-	checkWin: function(player) {
-		for(var i = 0; i < winningCombinations.length; i++){
-			if(board[winningCombinations[i][0]] == player && 
-			   board[winningCombinations[i][1]] == player &&
-			   board[winningCombinations[i][2]] == player) {
-				return true;
+	/*
+	 * Returns:
+	 * 'X' = X Wins
+	 * 'O' = O Wins
+	 * 'T' = Tie
+	 * 'N' = No winner yet
+	 *
+	checkWin: function() {
+
+		// Check for winning player
+		for(var i = 0; i < winningCombinations.length; i++) {
+			var tile1 = board[winningCombinations[i][0]];
+			var tile2 = board[winningCombinations[i][1]];
+			var tile3 = board[winningCombinations[i][2]];
+
+			if(tile1 == tile2 && tile2 == tile3 && tile1 != 'N') {
+				gameOver = true;
+				return tile1;
 			}
 		}
-		return false;
+
+
+		// Check for all tiles used
+		for(var i = 0; i < 9; i++) {
+			if(board[i] == 'N') {
+				return 'N';
+			}
+		}
+
+
+		// No winners and all tiles are used
+		gameOver = true;
+		return 'T'
 	}
 };
+*/
---
+++ gameLogic.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+	var mainBoard;// = TicTacToe;
+	var boards = new Array(9);
+	var nextBoard = 0; // The next board that must be played on (0 = any)
+	var	currentPlayer = 'X';
+
+
+	// Initialize the boards
+	mainBoard = new TicTacToe();
+	mainBoard.start();
+	for(var i = 0; i < 9; i++) {
+		boards[i] = new TicTacToe();
+		boards[i].start();
+	}
+
+
+	$('.tile').click(function() {
+		$(this).html(currentPlayer);
+		
+		clickedTile = $(this).attr("id").substring(0, 1);
+		nextBoard = $(this).attr("id").substring(1);
+
+		boards[clickedTile - 1].performTurn(nextBoard, currentPlayer);
+		swapTurns();
+		refreshBoards();
+
+		$("#currentTurn").html("It's " + currentPlayer + "'s Turn");
+	});
+
+
+	var swapTurns = function() {
+		if(currentPlayer == 'X') { currentPlayer = 'O'; }
+		else { currentPlayer = 'X'; }
+	};
+
+
+	// Enable/disable the boards that are available to be played on
+	var refreshBoards = function() {
+		
+		// Disable all boards/tiles
+		$(".largeTile").removeClass("validBoard");
+		$(".tile").removeClass("validTile");
+
+
+		// Enable valid board(s)
+		if(boards[nextBoard - 1].gameOver === true) {
+
+			// Enable other boards if the next board to be played on is invalid
+			for(var i = 1; i <= 9; i++) {
+				if(boards[i - 1].gameOver === false) {
+					$("#" + i + ".largeTile").addClass("validBoard");
+					enableBoard(i);
+				}
+			}
+		} else {
+			$("#" + nextBoard + ".largeTile").addClass("validBoard");
+			enableBoard(nextBoard);
+		}
+	}
+
+
+	// Enables a board and any valid tiles on it
+	var enableBoard = function(board) {
+		for(var i = 1; i <= 9; i++) {
+			if(boards[board - 1].isValidMove(i) === true) {
+				$("#" + board + "" + i + ".tile").addClass("validTile");
+			}
+		}
+	}
+});

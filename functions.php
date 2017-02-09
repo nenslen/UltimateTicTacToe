e8f68032251f0d7aaa0869846d5f1ca282dba66b
nenslen <nicolas.enslen@gmail.com>
2017-02-09 01:41:21 -0700

    Added tile class

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 			// 3 Large tiles
 			for($j = 1; $j <= 3; $j++) {
-				echo '<div class="largeTile" id="' . $id1 . '">';
+				echo '<div class="largeTile validBoard" id="' . $id1 . '">';
 
 				// 3 Small rows
 				$id2 = 1;
@@ -19,20 +19,15 @@
 
 					// 3 Small tiles
 					for($l = 1; $l <= 3; $l++) {
-						//echo '<div class="tile" id="' . $id1 . $id2++ .'">X</div>';
-						echo '<div class="tile" id="' . $id1 . $id2++ .'">X</div>';
+						echo '<div class="tile validTile" id="' . $id1 . $id2++ .'"></div>';
 					}
-
 					echo '</div>';
 				}
-
 				$id1++;
 				echo '</div>';
 			}
-
 			echo '</div>';
 		}
-
 		echo '</div>';
 	}
 ?>

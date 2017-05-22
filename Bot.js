0493ba997812a2a089f081d98898042f0a7af3c3
nenslen <nicolas.enslen@gmail.com>
2017-05-21 23:00:29 -0600

    Added bot object

---
+++ Bot.js
@@ -0,0 +1,18 @@
+function Bot(player) {
+
+	this.player = player;
+
+
+	this.getMove = function(board) {
+		console.log(board);
+		return "3";
+	};
+
+
+	// Returns which tile will result in a win (-1 if not possible)
+	this.findWinMove = function(board) {
+		for(var i = 1; i <= 9; i++) {
+			board.
+		}
+	};
+};
